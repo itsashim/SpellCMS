1c5c566efe97caeff7e0389c1f15fc11d960cc4e
itsashim <ashimrai903@gmail.com>
2025-05-21 16:40:09 +0545

    feat: dashboard search and filter functionality added

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -21,21 +21,62 @@ interface Post {
 function Dashboard() {
   const {data:posts = []} = usePosts();
   const [searchTerm, setSearchTerm] = useState("");
+  const [statusFilter, setStatusFilter] = useState("all");
+  const [categoryFilter, setCategoryFilter] = useState("all");
 
   const filteredPosts = posts.filter((post) => {
-    const lowerSearch = searchTerm.toLowerCase();
-    return (
-      post.title.toLowerCase().includes(lowerSearch) ||
-      post.tags.some((tag) => tag.toLowerCase().includes(lowerSearch))
-    );
+    // Search for Tag and Title
+    const searchMatch =
+      post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      post.tags.some((tag) => tag.toLowerCase().includes(searchTerm.toLowerCase()));
+
+      // Filter Status
+      const statusMatch = statusFilter === "all" || post.status === statusFilter;
+      // Filter Category
+    const categoryMatch = categoryFilter === "all" || post.category === categoryFilter;
+
+    return searchMatch && statusMatch && categoryMatch;
   });
 
+  const filteredCategories: string[] = [];
+    posts.forEach(post => {
+      if (!filteredCategories.includes(post.category)) {
+        filteredCategories.push(post.category);
+      }
+    });
   return (
     <DashboardLayout>
       <div className="flex max-w-[1600px] mx-auto justify-between mb-7">
-        <div>
-            <input className="form-input" type="text" placeholder="Search blog..." value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)} />
+        <div className="flex">
+          <input
+            className="form-input"
+            type="text"
+            placeholder="Search blog..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}/>
+
+          <select
+            className="form-select"
+            value={statusFilter}
+            onChange={(e) => setStatusFilter(e.target.value)}
+          >
+            <option disabled>Status</option>
+            <option value="all">All</option>
+            <option value="draft">Draft</option>
+            <option value="published">Published</option>
+          </select>
+
+          <select
+            className="form-select"
+            value={categoryFilter}
+            onChange={(e) => setCategoryFilter(e.target.value)}
+          >
+            <option disabled>Categories</option>
+            <option value="all">All</option>
+            {filteredCategories.map((category,index) => (
+              <option key={index} value={category as string}>{category as string}</option>
+            ))}
+          </select>
         </div>
         <Link to="/create-post"  className='btn-create ms-auto'>
           <IoAddSharp className="text-2xl text-white"/>
