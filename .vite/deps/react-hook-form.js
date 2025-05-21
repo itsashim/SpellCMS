7f4e71a29c684761d9e10c482e1afebf91fd6972
itsashim <ashimrai903@gmail.com>
2025-05-22 03:12:51 +0545

    feat: final submission

---
+++ .vite/deps/react-hook-form.js
@@ -0,0 +1,32 @@
+import {
+  Controller,
+  Form,
+  FormProvider,
+  appendErrors,
+  createFormControl,
+  get,
+  set,
+  useController,
+  useFieldArray,
+  useForm,
+  useFormContext,
+  useFormState,
+  useWatch
+} from "./chunk-SZLKCRUX.js";
+import "./chunk-DQW77RHJ.js";
+import "./chunk-4MBMRILA.js";
+export {
+  Controller,
+  Form,
+  FormProvider,
+  appendErrors,
+  createFormControl,
+  get,
+  set,
+  useController,
+  useFieldArray,
+  useForm,
+  useFormContext,
+  useFormState,
+  useWatch
+};
---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# SpellCMS
+A functional CMS dashboard with authentication, built using React, TypeScript, Tailwind CSS, and tested with Jest & React Testing Library.
+
